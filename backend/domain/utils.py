c0856d25d80f054519e659562298b62442972f7a
Adam Kukučka <xkukuck1@fi.muni.cz>
2024-11-02 16:09:27 +0100

    feat: duplicates

--- backend/domain/utils.py
+++ backend/domain/utils.py
@@ -6,3 +6,8 @@ import pandas as pd
 def extreme_timestamps(file: str) -> tuple[datetime, datetime]:
     df = pd.read_csv(file, header=1, index_col=0)
     return df.index.min(), df.index.max()
+
+
+def duplicites(file: str) -> int:
+    df = pd.read_csv(file, header=1, index_col=0)
+    return df.index.duplicated().sum()
